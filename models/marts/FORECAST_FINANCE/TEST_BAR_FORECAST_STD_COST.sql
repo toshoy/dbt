269232aeeabd436b8d4d608a41f60651ee184f22
toshoy <cesarrodriguez10@gmail.com>
2024-11-08 20:48:10 -0500

    Rolling avg

--- models/marts/FORECAST_FINANCE/TEST_BAR_FORECAST_STD_COST.sql
+++ models/marts/FORECAST_FINANCE/TEST_BAR_FORECAST_STD_COST.sql
@@ -7,11 +7,27 @@ std_cost_gpp as (
     dmd.*,
     std.UNIT_STD_MAT_COST,
     std."%_FREIGHT",
-    gpp.GPP_DIVISION_CODE
+    gpp.GPP_DIVISION_CODE,
+    mrkt.REGION,
+    mrkt."MARKET CHANNEL",
+    mrkt."ROC Dmd Grp"
     from demand AS dmd
     left join {{ ref('TEST_BAR_STD_COST_FREIGHT') }}  AS std ON dmd.SKU_KEY = std.PRODUCT_MATERIAL_CODE
     left join {{ ref('_stg_DIM_GPP') }} as gpp on dmd.GPP_KEY = gpp.GPP_KEY
     left join {{ ref('_stg_Dim_Con_Market_channel') }} as mrkt on mrkt.mrkt_chnl_key = dmd.mrkt_chnl_key
-    )
+    ),
+bar_key as (
+    select *,
+    IFF( 
+        "REGION" = 'NORTH AMERICA', 
+        UPPER((CONCAT(COALESCE(TRIM("GPP_DIVISION_CODE"),''), COALESCE(TRIM("MARKET CHANNEL"),'')))),
+        UPPER(CONCAT(COALESCE(TRIM("GPP_DIVISION_CODE"),''), COALESCE(TRIM("ROC Dmd Grp"),'')))) AS "BAR_KEY"
+    from std_cost_gpp),
+pnl_join as (
+    select dmd.*,
+    pnl."RSA_%_{{var('rolling_avg')}}_MONTHS",
+    pnl."TOTAL_GTN_%_{{var('rolling_avg')}}_MONTHS"
+    from bar_key as dmd
+    left join {{ ref('_2_rolling_avg') }} as pnl on dmd.bar_key = pnl.bar_key )
 SELECT *
-FROM std_cost_gpp
+FROM pnl_join
---
+++ models/refinement/FORECAST_FINANCE/BAR_PnL/_2_rolling_avg.sql
@@ -0,0 +1,30 @@
+with sources as (
+    select *,
+    (select DATEADD(MONTH,{{var('rolling_avg')}},MAX(DATE_AS_OF)) AS MIN_DATE from {{ ref('TEST_BAR_PnL') }}) as MIN_DATE,
+    (select max(DATE_AS_OF) from {{ref('TEST_BAR_PnL')}}) AS "MAX_DATE"
+    from {{ ref('TEST_BAR_PnL') }}),
+filtering as (
+    SELECT
+    bar_key,
+    sum("GROSS SALES") AS "GROSS SALES {{var('rolling_avg')}} MONTHS",
+    SUM(RSA) AS "RSA {{var('rolling_avg')}} MONTHS",
+    SUM("DISCOUNTS AND ALLOWANCES") AS "DaA {{var('rolling_avg')}} MONTHS",
+    SUM("FILL RATE FINES") AS "FILL RATE FINES {{var('rolling_avg')}} MONTHS",
+    SUM("GTN OTHER") AS "GTN OTHER {{var('rolling_avg')}} MONTHS",
+    SUM("TOTAL GTN") AS "TOTAL GTN {{var('rolling_avg')}} MONTHS",
+    FROM sources
+    where DATE_AS_OF >= MIN_DATE
+    GROUP BY ALL),
+FINAL AS (
+    SELECT *,
+    div0("RSA {{var('rolling_avg')}} MONTHS","GROSS SALES {{var('rolling_avg')}} MONTHS") AS "RSA_%_{{var('rolling_avg')}}_MONTHS",
+    div0("DaA {{var('rolling_avg')}} MONTHS","GROSS SALES {{var('rolling_avg')}} MONTHS") AS "DaA_%_{{var('rolling_avg')}}_MONTHS",
+    div0("FILL RATE FINES {{var('rolling_avg')}} MONTHS","GROSS SALES {{var('rolling_avg')}} MONTHS") AS "FILL_RATE_%_{{var('rolling_avg')}}_MONTHS",
+    div0("GTN OTHER {{var('rolling_avg')}} MONTHS","GROSS SALES {{var('rolling_avg')}} MONTHS") AS "GTN_OTHER_%_{{var('rolling_avg')}}_MONTHS",
+    div0("TOTAL GTN {{var('rolling_avg')}} MONTHS","GROSS SALES {{var('rolling_avg')}} MONTHS") AS "TOTAL_GTN_%_{{var('rolling_avg')}}_MONTHS"
+    FROM filtering)
+select *
+from FINAL
+
+
+

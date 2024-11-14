26b37f35daa98354b5bf084bebad014e5234e8ec
toshoy <cesarrodriguez10@gmail.com>
2024-11-14 10:40:25 -0500

    financials in usd

--- models/marts/FORECAST_FINANCE/TEST_BAR_FORECAST_STD_COST.sql
+++ models/marts/FORECAST_FINANCE/TEST_BAR_FORECAST_STD_COST.sql
@@ -25,13 +25,70 @@ bar_key as (
     from std_cost_gpp),
 pnl_join as (
     select dmd.*,
+    pnl."DaA_%_{{var('rolling_avg')}}_MONTHS",
+    pnl."FILL_RATE_%_{{var('rolling_avg')}}_MONTHS",
+    pnl."GTN_OTHER_%_{{var('rolling_avg')}}_MONTHS",
     pnl."RSA_%_{{var('rolling_avg')}}_MONTHS",
     pnl."TOTAL_GTN_%_{{var('rolling_avg')}}_MONTHS"
     from bar_key as dmd
     left join {{ ref('_2_rolling_avg') }} as pnl on dmd.bar_key = pnl.BAR_KEY_ADJ ),
 final as (
-    select *
-    from pnl_join
-)
+    select
+    "SRC_SYS_KEY",
+    "CURR_RCRD_FLAG",
+    "DMD_FCST_CTGY",
+    "MRKT_CHNL_KEY",
+    "SKU_KEY",
+    "LOC_KEY",
+    "FCST_DUR",
+    "FCST_ID_TYP",
+    "FCST_TYP",
+    "FCST_START_DTE",
+    "PLNG_MODEL_NAME",
+    "F_MONTH_DATE",
+    "ETL_INS_DTE",
+    "PRC_PRFL_UNIT_GSV",
+    "DMD_GRP_UNIT_GSV",
+    "DMD_GRP_ROLLUP_UNIT_GSV",
+    "DMD_GRP_BUS_REGN_UNIT_GSV",
+    "HIST_START_DATE",
+    "HIST_END_DATE",
+    "HIST_UNIT_GSV",
+    "Avg_CRNCY_RECENT_PRICE",
+    "RECENT_PRC_PRFL_UNIT_GSV",
+    "HIST_AND_RCNT_PRICE_UNIT_GSV",
+    "FCST_QTY",
+    "FCST_GSV",
+    ROUND("UNIT_STD_MAT_COST"*"FCST_QTY",2) AS "COS_STD_COST",
+    ROUND("%_FREIGHT"*"FCST_GSV",2) AS "COS_OB_FREIGHT",
+    ROUND("RSA_%_-6_MONTHS"*"FCST_GSV",2) AS "RSA",
+    ROUND("FILL_RATE_%_-6_MONTHS"*"FCST_GSV",2) AS "GTN_FILL_RATE_FINES",
+    ROUND("DaA_%_-6_MONTHS"*"FCST_GSV",2) AS "GTN_DISC_ALLOW",
+    ROUND("GTN_OTHER_%_-6_MONTHS"*"FCST_GSV",2) AS "GTN_OTHER",
+    ROUND("TOTAL_GTN_%_-6_MONTHS"*"FCST_GSV",2) AS "GTN_TOTAL",
+    "RUN_DATE",
+    "DMD_GRP_TYP",
+    "PLNG_LVL",
+    "DFU_LVL",
+    "SRC_SF_TBL_KEY",
+    "PROD_KEY",
+    "PROGRAM ID",
+    "Yearly Outlook",
+    "FUZZY_FLAG",
+    "GPP_KEY",
+    "BRAND_KEY",
+    "UNIT_STD_MAT_COST",
+    "%_FREIGHT" AS "OB_FREIGHT_%",
+    "GPP_DIVISION_CODE",
+    "REGION",
+    "MARKET CHANNEL",
+    "ROC Dmd Grp",
+    "BAR_KEY" as "DIV_MRKT_KEY",
+    --"DaA_%_-6_MONTHS",
+    --"FILL_RATE_%_-6_MONTHS",
+    --"GTN_OTHER_%_-6_MONTHS",
+    "RSA_%_-6_MONTHS"
+    --"TOTAL_GTN_%_-6_MONTHS"
+    from pnl_join)
 SELECT *
 FROM final
--- models/refinement/FORECAST_FINANCE/STD_COST_FREIGHT/1_ETL.sql
+++ models/refinement/FORECAST_FINANCE/STD_COST_FREIGHT/1_ETL.sql
@@ -4,6 +4,13 @@ with source as (
         round(cos_st_cost,2) AS "COS_ST_COST_FIX"
         from {{ ref('_stg__VW_PROFIT') }}
         where round(cos_st_cost,2) <> 0 and "NET_SHIP_QTY" <> 0),
+date_filter as (
+    select *,
+    (select max("DATE_AS_OF") from source ) as "MAX_DATE",
+    (select DATEADD(MONTH,{{var('rolling_avg')}},MAX("DATE_AS_OF")) AS MIN_DATE FROM source) as "MIN_DATE"
+    from source
+    WHERE "DATE_AS_OF" >= (select DATEADD(MONTH,{{var('rolling_avg')}},MAX("DATE_AS_OF")) AS MIN_DATE FROM source)
+),
 trans as (
     SELECT 
     product_material_code,
@@ -13,7 +20,7 @@ trans as (
     ROUND(sum(CO_OB_FREIGHT),2) AS "FREIGHT",
     ROUND(sum(NET_SHIP_GSV_W_O_RSA),2) AS "GSV_W_O_RSA",
     round(sum("COS_ST_COST_FIX"),2) as "COS_ST_COST"
-    FROM source
+    FROM date_filter
     GROUP BY ALL),
 final as (
     SELECT
@@ -22,10 +29,10 @@ final as (
     "MAX_DATE_AS_OF",
     "NET_SHIP_QTY",
     "COS_ST_COST",
-    IFF(div0("COS_ST_COST","NET_SHIP_QTY") < 0 , 0 ,round(div0("COS_ST_COST","NET_SHIP_QTY"),2))  as "UNIT_STD_MAT_COST", --excluding negative values
+    IFF(div0("COS_ST_COST","NET_SHIP_QTY") < 0 , 0 ,round(div0("COS_ST_COST","NET_SHIP_QTY"),3))  as "UNIT_STD_MAT_COST", --excluding negative values
     "FREIGHT",
     "GSV_W_O_RSA",
-    IFF(div0("FREIGHT","GSV_W_O_RSA") < 0 , 0 ,round(div0("FREIGHT","GSV_W_O_RSA"),2))  as "%_FREIGHT" --excluding negative values
+    IFF(div0("FREIGHT","GSV_W_O_RSA") < 0 , 0 ,round(div0("FREIGHT","GSV_W_O_RSA"),4))  as "%_FREIGHT" --excluding negative values
     from trans)
 SELECT *
 FROM final
